cdb41e5ede1a4654695a8d5242840b9082ce08c9
sandeshrajgurung <102845333+sandeshrajgurung@users.noreply.github.com>
2024-10-28 17:09:25 +0545

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("IT WORKED");
+  res.send("My Student ID is 22003340 Welcome to Express");
 });
 app.get("/new", function (req, res) {
   res.send("New also worked");
